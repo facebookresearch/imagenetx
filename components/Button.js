34405a8c701c10b7f1b25bee181b4138bbb79caf
William Ngan <williamngan@users.noreply.github.com>
2022-11-01 02:10:35 -0700

    First pass clean-up

--- components/Button.js
+++ components/Button.js
@@ -1,19 +1,16 @@
 const Site = require("../site.json");
 import Link from "next/link";
+import { RiArrowRightUpLine } from "react-icons/ri";
 
-export default function Button({ url, children, color, small, style }) {
+export default function Button({ url, children, color, small, openNew, style }) {
   const c = color || `${Site.theme}-${Site.shade + 100}`;
   const size = small ? "px-4 py-2 text-sm" : "px-6 py-4 text-md";
+  const classes = `flex rounded-md no-underline items-center ${openNew ? 'gap-2' : ''} justify-center font-semibold text-white hover:text-${c} hover:shadow-md bg-${c} hover:bg-white ${size}`;
 
   return (
     <span className="comp_button inline-block" style={style}>
-      <Link href={url} passHref>
-        <a
-          className={`flex rounded-md no-underline items-center justify-center font-semibold text-white hover:text-${c} hover:shadow-md bg-${c} hover:bg-white ${size}`}
-        >
-          {children}
-        </a>
-      </Link>
+      {!openNew && <Link href={url} passHref><a className={classes}>{children}</a></Link>}
+      {openNew && <a href={url} target="_blank" rel="noopener" className={classes}>{children} <RiArrowRightUpLine /></a>}
     </span>
   );
 }
